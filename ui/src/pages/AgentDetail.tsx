b3f26bc0d055415c2643a949fa464b431cf6225c
zocomputer <help@zocomputer.com>
2026-06-10 13:10:03 +0000

    Expose prompt template config for all adapters

--- ui/src/pages/AgentDetail.tsx
+++ ui/src/pages/AgentDetail.tsx
@@ -1748,7 +1748,6 @@ function AgentConfigurePage({
         onSavingChange={onSavingChange}
         updatePermissions={updatePermissions}
         companyId={companyId}
-        hidePromptTemplate
         hideInstructionsFile
       />
       <div>
