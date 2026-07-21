49988d2f0110240b2d2dc4164c3a6409fb05272d
Jannes Stubbemann <jannes.stubbemann@gmail.com>
2026-07-21 12:24:29 +0200

    feat(k8s-provider): egressPolicy open-internet mode with hardened CIDR excepts

--- packages/plugins/sandbox-providers/kubernetes/src/plugin.ts
+++ packages/plugins/sandbox-providers/kubernetes/src/plugin.ts
@@ -247,6 +247,7 @@ const plugin = definePlugin({
       paperclipServerNamespace: PAPERCLIP_SERVER_NAMESPACE,
       serviceAccountAnnotations: config.serviceAccountAnnotations,
       egressMode: config.egressMode,
+      egressPolicy: config.egressPolicy,
       egressAllowFqdns: [...adapterDefaults.allowFqdns, ...config.egressAllowFqdns],
       egressAllowCidrs: config.egressAllowCidrs,
       resourceQuota: DEFAULT_RESOURCE_QUOTA,
